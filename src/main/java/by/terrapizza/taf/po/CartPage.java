66d43a15abf69ef5b306a98fa5ad82d11285025c
Olga Bernat <olga.alexenok@gmail.com>
2023-09-13 12:29:33 +0200

    update CartPage test

--- src/main/java/by/terrapizza/taf/po/CartPage.java
+++ src/main/java/by/terrapizza/taf/po/CartPage.java
@@ -3,14 +3,16 @@ package by.terrapizza.taf.po;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class CartPage {
     private WebDriver driver;
-    private By cartButton = By.xpath("//div[@class='basket__btn-top basket__top basket__btn-top--sm']");
+    private By cartButton = By.xpath("//div[@class='basket__top-right']");
     private By firstProductInCartText = By.xpath("//div[@class='basket__products-item-name'][contains(text(), 'Пицца Маргарита')]");
     private By secondProductInCartText = By.xpath("//div[@class='basket__products-item-name'][contains(text(), 'Латте')]");
 
@@ -26,10 +28,11 @@ public class CartPage {
     public List<String> getProductNames() {
         List<String> productNames = new ArrayList<>();
 
-        WebElement firstProductElement = driver.findElement(firstProductInCartText);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement firstProductElement = wait.until(ExpectedConditions.visibilityOfElementLocated(firstProductInCartText));
         productNames.add(firstProductElement.getText());
 
-        WebElement secondProductElement = driver.findElement(secondProductInCartText);
+        WebElement secondProductElement = wait.until(ExpectedConditions.visibilityOfElementLocated(secondProductInCartText));
         productNames.add(secondProductElement.getText());
 
         return productNames;
